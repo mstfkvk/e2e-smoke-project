784e5a83784f64aaafbcdd715ae494857702fa1b
mstfkvk <mstfkvk5600@gmail.com>
2026-04-16 00:57:39 +0300

    workflow is added

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,9 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
