ed2090b2d8674bbdf82e7d8c96164498966160ed
mstfkvk <mstfkvk5600@gmail.com>
2026-04-16 01:24:41 +0300

    workflow is added3

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/html',  // workflow'daki publish_dir ile aynı olmalı
+    overwrite: true,
+    html: true,
+    json: false,
+    inlineAssets: true,  // ← CSS/JS'i HTML'e gömer, Pages'de çalışması için şart
+    embeddedScreenshots: true,
+  },
   e2e: {
-    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
